a9211d28d731c4b58cfa8585a612aa838f63142c
blackbox <blackbox@870081.local>
2023-09-15 23:04:06 +0700

    Add dependecies Zustand for state management

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -32,12 +32,12 @@ const Button: React.FC<ButtonProps> = ({
                 transition
                 w-full
                 ${outline ? 'bg-white' : 'bg-rose-500'}
-        ${outline ? 'border-black' : 'border-rose-500'}
-        ${outline ? 'text-black' : 'text-white'}
-        ${small ? 'text-sm' : 'text-md'}
-        ${small ? 'py-1' : 'py-3'}
-        ${small ? 'font-light' : 'font-semibold'}
-        ${small ? 'border-[1px]' : 'border-2'}
+                ${outline ? 'border-black' : 'border-rose-500'}
+                ${outline ? 'text-black' : 'text-white'}
+                ${small ? 'text-sm' : 'text-md'}
+                ${small ? 'py-1' : 'py-3'}
+                ${small ? 'font-light' : 'font-semibold'}
+                ${small ? 'border-[1px]' : 'border-2'}
             `}
     >
       {Icon && (
