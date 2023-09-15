68335e2fab2a6c01502eba8aa14be218b1330b9b
blackbox <blackbox@870081.local>
2023-09-15 23:00:57 +0700

    Create Navbar and Modal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
-import Navbar from './components/navbar/Navbar'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
+import Navbar from './components/navbar/Navbar';
+import Modal from './components/modals/Modal';
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'AirBnB',
   description: 'Air BnB Clone',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-      <Navbar />
+        <Modal actionLabel='Submit' title='Hello Word' isOpen />
+        <Navbar />
         {children}
       </body>
     </html>
-  )
+  );
 }
